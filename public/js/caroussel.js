bb1a51206add4027a8dc05e85eb042516885d951
Joshuavm <joshuavanmeer@gmail.com>
2016-09-14 20:08:11 +0200

    added swipe functionality

--- public/js/caroussel.js
+++ public/js/caroussel.js
@@ -78,7 +78,7 @@ var caroussel = (function(w,d){
                 moveSlide(state.activeSlide - 1);
             } else if (target.id.indexOf('UI-pin-') !== -1) {
                 var id = target.id.replace('UI-pin-', '');
-                moveSlide('', Number(id));
+                moveSlide(Number(id));
             }
         }
 
@@ -102,29 +102,25 @@ var caroussel = (function(w,d){
                     state.touchEndX = touchX;
 
                     if (Math.abs(state.touchStartX - state.touchEndX) > 100 &&
-                        state.touchStartX - state.touchEndX > 0) {
-                            moveSlide('right', state.activeSlide + 1);
+                        state.touchStartX - state.touchEndX > 0 &&
+                        state.activeSlide !== state.totalSlides - 1) {
+                            moveSlide(state.activeSlide + 1);
                     }
                     else if (Math.abs(state.touchStartX - state.touchEndX) > 100 &&
-                        state.touchStartX - state.touchEndX < 0) {
-                            moveSlide('left', state.activeSlide - 1);
+                        state.touchStartX - state.touchEndX < 0 &&
+                        state.activeSlide !== 0) {
+                            moveSlide(state.activeSlide - 1);
                     }
                     break;
-
             }
         }
 
 
 
 
-        function moveSlide(direction, slots) {
-            if ((direction === 'right' && state.activeSlide !== (state.totalSlides - 1)) ||
-                (direction === 'left' && state.activeSlide !== 0)) {
-                slideContainer.style = '-' + (slots * state.width) + 'px';
-            }
-            else if (!direction) {
-                slideContainer.style = '-' + (slots * state.width) + 'px';
-            }
+        function moveSlide(slots) {
+            slideContainer.style.left = '-' + (slots * state.width) + 'px';
+
             //update active state on sliderCollection and apply change to state
             sliderCollection.updateActive(slots);
             state.activeSlide = sliderCollection.getActive();
